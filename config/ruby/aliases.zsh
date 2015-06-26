1ccb3fa143ebd6512d28f3efb24c1bbc8a818c17
kirisu <github@kirisu.co.uk>
2015-06-26 18:04:33 +0100

    Tweaks.

--- config/ruby/aliases.zsh
+++ config/ruby/aliases.zsh
@@ -1,9 +1,7 @@
 alias -g bi="bundle install --path=vendor"
 alias -g be="bundle exec"
-alias -g rake="noglob be rake"
-alias -g rs="be rails s"
-alias -g rsp="be rspec"
+alias -g rake="noglob bundle exec rake"
+alias -g rs="bundle exec rails s"
+alias -g rsp="bundle exec rspec"
 
 alias -g flog="find app -name \*.rb | xargs flog"
-alias -g coverage="open -a \"Google Chrome\" coverage/index.html"
-alias -g all="be rake && flog && rubocop -a"
